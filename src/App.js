828c2a19f9f8c1be17405a721c8f8c4a40178d33
WaelS4 <wael.sellami.44@gmail.com>
2021-06-30 13:39:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import Profile from './Component/Profile';
+import Compteur from './Component/Compteur';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = { etatBt: false, }
+    handClick = () => {
+        this.setState({
+            etatBt: !this.state.etatBt
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handClick}>{this.state.etatBt ? "Masqué le Profile" : "Affiche le Profile"}</Button>
+                {this.state.etatBt ? <div><Profile /><br />Compteur :  <Compteur /></div> : ""}
+
+            </div >
+        );
+    }
 }
-
-export default App;
